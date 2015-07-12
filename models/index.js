1ccbf7c4808620047ef318a03041f094646b95d9
Annie Cook <anniecook@ostby.com>
2015-07-12 16:21:45 -0400

    all setup

---
+++ models/index.js
@@ -0,0 +1,29 @@
+var mongoose = require('mongoose');
+// Notice the `mongodb` protocol; Mongo is basically a kind of server,
+// which handles database requests and sends responses. It's async!
+mongoose.connect('mongodb://localhost/on-my-route');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongodb connection error:'));
+
+var addressSchema = new mongoose.Schema({
+  name: String,
+  address: String
+});
+
+var tripSchema = new mongoose.Schema({
+  start: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Address'
+  },
+  end: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Address'
+  },
+  route_name: String
+})
+
+
+module.exports = {
+  Address: mongoose.model('Address', addressSchema),
+  Trip: mongoose.model('Trip', tripSchema)
+};
